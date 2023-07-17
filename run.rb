300e58609e189a27698be82e2b2d753ab5d8633c
Kieran Klaassen <kieranklaassen@gmail.com>
2023-07-17 10:46:07 -0500

    feat: move pr template as an option to set

--- run.rb
+++ run.rb
@@ -3,6 +3,7 @@ require_relative 'lib/pull_request_description_service'
 service = PullRequestDescriptionService::Generator.new(
   ENV['REPO'],
   ENV['PR_NUMBER'],
-  ENV['GITHUB_REPOSITORY']
+  ENV['GITHUB_REPOSITORY'],
+  ENV['PULL_REQUEST_TEMPLATE']
 )
 service.run
